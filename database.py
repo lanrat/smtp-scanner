4d87751cb0cbd6aab9901413d3357de5ec670677
Jon Larson <j1larson@ucsd.edu>
2014-03-05 18:36:15 -0800

    database complete

--- database.py
+++ database.py
@@ -13,9 +13,9 @@ class database:
         print "SQlite version %s" % data
 
         # Create initial tables
-        self.cur.execute("CREATE TABLE IF NOT EXISTS Domains(Id INTEGER PRIMARY KEY, Domain TXT);")
-        self.cur.execute("CREATE TABLE IF NOT EXISTS Mx(Id INTEGER PRIMARY KEY, Domain_id INT, Domain TXT, Priority INT);")
-        self.cur.execute("CREATE TABLE IF NOT EXISTS Server(Id INTEGER PRIMARY KEY, Mx_id INT, IP TXT, ESMTP BIT, TLS BIT, \
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Domains(Id INTEGER PRIMARY KEY AUTOINCREMENT, Domain TXT);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Mx(Id INTEGER PRIMARY KEY AUTOINCREMENT, Domain_id INT, Domain TXT, Priority INT);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Server(Id INTEGER PRIMARY KEY AUTOINCREMENT, Mx_id INT, IP TXT, ESMTP BIT, TLS BIT, \
                 SSL_Ciper_Name TXT, SSL_Cipher_Version TXT, SSL_Cipher_Bits INT, SSL_Verified BIT);")
     
     def __del__(self):
@@ -26,7 +26,7 @@ class database:
     def addDomain(self, domain):
         """Add domain record
         
-        Arguments:
+        Paramaters:
             domain -- string of domain to add
         Return:
             id of new record
@@ -36,9 +36,38 @@ class database:
         return self.cur.lastrowid
 
 
-    def addMX(self, mx):
-        return
+    def addMX(self, domain_id, domain, priority):
+        """Add MX record
+        
+        Paramaters:
+            domain_id -- id of domain record
+            domain -- string of domain name
+            priority -- priority of mx record
+        Return:
+            id of new record
+        """
+
+        self.cur.execute("INSERT INTO Mx VALUES" \
+                "(NULL, %d, '%s', %d);" % (domain_id, domain, priority))
+        return self.cur.lastrowid
 
 
-    def addServer(self, serv):
-        return
+    def addServer(self, mx_id, serv):
+        """Add Server record
+        
+        Paramaters:
+            mx_id -- id of MX record
+            serv -- smtp_server object
+        Return:
+            id of new record
+        """
+
+        if serv is None:
+            return -1
+
+        self.cur.execute("INSERT INTO Server VALUES" \
+                "(NULL, %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s');" \
+                % (mx_id, serv.ip, serv.esmtp, serv.tls, \
+                serv.ssl_cipher_name, serv.ssl_cipher_version, \
+                serv.ssl_cipher_bits, serv.ssl_verified))
+        return self.cur.lastrowid
