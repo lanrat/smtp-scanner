081eff6644adc1a8ba643237c52dca5320f2e9f6
Jon Larson <j1larson@ucsd.edu>
2014-03-05 17:54:25 -0800

    added database 'addDomain' function

--- database.py
+++ database.py
@@ -4,30 +4,41 @@ import sys
 class database:
 
     def __init__(self):
+        """Initialize the database if the tables have not already been created"""
         self.con = lite.connect('database.db')
-	cur = self.con.cursor()
+        self.cur = self.con.cursor()
 
-	cur.execute('SELECT SQLITE_VERSION()')
-	data = cur.fetchone()
-	print "SQlite version %s" % data
+        self.cur.execute('SELECT SQLITE_VERSION()')
+        data = self.cur.fetchone()
+        print "SQlite version %s" % data
 
-	# Create initial tables
-	cur.execute("CREATE TABLE IF NOT EXISTS Domains(Id INTEGER PRIMARY KEY, Domain TXT);")
-	cur.execute("CREATE TABLE IF NOT EXISTS Mx(Id INTEGER PRIMARY KEY, Domain_id INT, Domain TXT, Priority INT);")
-	cur.execute("CREATE TABLE IF NOT EXISTS Server(Id INTEGER PRIMARY KEY, Mx_id INT, IP TXT, ESMTP BIT, TLS BIT, \
-		SSL_Ciper_Name TXT, SSL_Cipher_Version TXT, SSL_Cipher_Bits INT, SSL_Verified BIT);")
-	
+        # Create initial tables
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Domains(Id INTEGER PRIMARY KEY, Domain TXT);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Mx(Id INTEGER PRIMARY KEY, Domain_id INT, Domain TXT, Priority INT);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Server(Id INTEGER PRIMARY KEY, Mx_id INT, IP TXT, ESMTP BIT, TLS BIT, \
+                SSL_Ciper_Name TXT, SSL_Cipher_Version TXT, SSL_Cipher_Bits INT, SSL_Verified BIT);")
+    
     def __del__(self):
-	if self.con: self.con.close()
-	
+        """Cleanup"""
+        if self.con: self.con.close()
+    
 
     def addDomain(self, domain):
-	return
+        """Add domain record
+        
+        Arguments:
+            domain -- string of domain to add
+        Return:
+            id of new record
+        """
+
+        self.cur.execute("INSERT INTO Domains(Domain) VALUES ('%s');" % domain)
+        return self.cur.lastrowid
 
 
     def addMX(self, mx):
-	return
+        return
 
 
     def addServer(self, serv):
-	return
+        return
